89bdd27cd6481ba1271def6755361dda8e1defa9
audiotrope <122730216+audiotrope@users.noreply.github.com>
2026-01-27 11:45:57 +0100

    Lab work

--- cars.java
+++ cars.java
@@ -2,86 +2,69 @@ import java.awt.*;
 
 import static java.lang.Math.*;
 
-public class cars implements Movable { //can't have the "abstract" while having a Testing class
-                                        //What
-    public int nrDoors; 
-    public double enginePower; 
-    public double[] currentSpeed = {0,0}; // The current speed of the car in x and y
+public abstract class cars implements Movable { //can't have the "abstract" while having a Testing class
+    //We can have abstract, because we will not be testing the car class, we will
+    // be testing subclasses.
+
+    public int nrDoors;
+    public double enginePower;
+    public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
     public String modelName; // The car model name
     public double x; // xcord
     public double y; //ycord
+    private double[] direction = {0,1};
+    private final double[][] directions = {{0,1},{1,0},{0,-1},{-1,0}}; //up -> right -> down -> left
+    private int dir_index = 0;
 
-    public int getNrDoors(){
+
+    public int getNrDoors () {
 
         return nrDoors;
     }
 
-    public double getEnginePower(){
+    public double getEnginePower() {
         return enginePower;
     }
 
-    public double getCurrentSpeed() {
-        double speed;
-        speed = sqrt( pow(currentSpeed[0],2) + pow(currentSpeed[1],2) );
-        return speed;
+    public double getCurrentSpeed () {
+        return currentSpeed;
     }
 
-    public Color getColor(){
+    public Color getColor () {
         return color;
     }
 
-    public void move() {
-        x = x + currentSpeed[0];
-        y = y + currentSpeed[1];
+    public void move () {
+        x = x + direction[0]*currentSpeed;
+        y = y + direction[1]*currentSpeed;
     }
 
-    public void turnLeft(){
-        turn(0.1d);
+    public void turnLeft () {
+        dir_index -=1; //dec Index. Next direction: counter clockwise
+        dir_index %=4; // mod 4 to keep it in range
+        direction = directions[dir_index]; //get direction
     }
 
-    public void turnRight(){
-        turn(-0.1d);
+    public void turnRight () {
+        dir_index +=1;
+        dir_index %=4;
+        direction = directions[dir_index];
     }
 
-    public void setColor(Color clr){
+    public void setColor (Color clr){
         color = clr;
     }
 
-    public void startEngine(){
-        currentSpeed = new double[]{0.1d, 0.1d};
+    public void startEngine () {
+        currentSpeed = 0.1;
     }
-    
-    public void stopEngine(){
-        currentSpeed = new double[]{0.0d,0.0d};
-    }
-    
-    private double[] to_polar(double x, double y) {
-        double angle;
-        double radius;
-        radius = sqrt(pow(x, 2.0d) + pow(y, 2.0d)); // Distance to origo
-        angle = atan2(x, y); // Angle from x-axis
-        double[] polar = {angle, radius};
-        return polar;
-    } //Converts a cartesian coordinate (x,y) to polar form (angle, radius), angle in radians
 
-    private double[] from_polar(double angle, double radius){
-        double x;
-        double y;
-        x = cos(angle) * radius;
-        y = sin(angle) * radius;
-        double[] coords = {x,y};
-        return coords;
-    } //Converts a polar coordinate to (x,y)
-    
-    private void turn(double angle){
-        double x = this.currentSpeed[0];
-        double y = this.currentSpeed[1];
-        double[] polar = to_polar(x, y);
-        polar[0] += angle;
-        polar[0] %= 2.0d*PI;
-        double[] rotatedSpeed = from_polar(polar[0], polar[1]);
-        this.currentSpeed = rotatedSpeed;
-    } // Negative = Clockwise, Positive = Counter-clockwise
+    public void stopEngine () {
+        currentSpeed = 0;
+    }
+
+    public double[] getDirection(){
+        return direction;
+    }
 }
-
