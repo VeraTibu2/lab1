1cb46fdc9c6e77a354d04d741d9211a8b1c7240f
verat <veratibucia@gmail.com>
2026-02-16 15:58:33 +0100

    new lab and new files

--- CarController.java
+++ CarController.java
@@ -21,7 +21,7 @@ public class CarController {
     // The frame that represents this instance View of the MVC pattern
     CarView frame;
     // A list of cars, modify if needed
-    ArrayList<Cars> Cs = new ArrayList<>();
+    ArrayList<Cars> cars = new ArrayList<>();
 
     //methods:
 
@@ -29,7 +29,7 @@ public class CarController {
         // Instance of this class
         CarController cc = new CarController();
 
-        cc.Cs.add(new Saab95());
+        cc.cars.add(new Volvo240());
 
         // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
@@ -43,7 +43,7 @@ public class CarController {
     * */
     private class TimerListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            for (Cars car : Cs) {
+            for (Cars car : cars) {
                 car.move();
                 int x = (int) Math.round(car.x);
                 int y = (int) Math.round(car.y);
@@ -57,17 +57,16 @@ public class CarController {
     // Calls the gas method for each car once
     void gas(int amount) {
         double gas = ((double) amount) / 100;
-       for (Cars car : Cs
+       for (Cars car : cars
                 ) {
             car.gas(gas);
        }
     }
 
-    void break(double amount) {
-        double b = ((double) amount) / 100;
-        for (Cars car : Cs
-        ) {
-            car.brake(b);
+    void Break(int amount) {
+        double Br = ((double) amount) / 100;
+        for (Cars car : cars) {
+            car.brake(Br);
         }
     }
 }
--- CarView.java
+++ CarView.java
@@ -29,6 +29,12 @@ public class CarView extends JFrame{
     int gasAmount = 0;
     JLabel gasLabel = new JLabel("Amount of gas");
 
+    JPanel BreakPanel = new JPanel();
+    JSpinner breakSpinner = new JSpinner();
+    int BreakAmount = 0;
+    JLabel breakLabel = new JLabel("Amount of break");
+
+
     JButton gasButton = new JButton("Gas");
     JButton brakeButton = new JButton("Brake");
     JButton turboOnButton = new JButton("Saab Turbo on");
@@ -75,6 +81,22 @@ public class CarView extends JFrame{
 
         this.add(gasPanel);
 
+        SpinnerModel spinnerModel1 =
+                new SpinnerNumberModel(0,0,100,1);
+        breakSpinner = new JSpinner((spinnerModel1));
+        breakSpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                BreakAmount = (int) ((JSpinner)e.getSource()).getValue();
+            }
+        });
+
+        BreakPanel.setLayout(new BorderLayout());
+        BreakPanel.add(breakLabel, BorderLayout.PAGE_START);
+        BreakPanel.add(breakSpinner, BorderLayout.PAGE_END);
+
+        this.add(BreakPanel);
+
         controlPanel.setLayout(new GridLayout(2,4));
 
         controlPanel.add(gasButton, 0);
@@ -111,7 +133,7 @@ public class CarView extends JFrame{
         brakeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                carC.break(5);
+                carC.Break(BreakAmount);
             }
         });
         // Make the frame pack all it's components by respecting the sizes if possible.
